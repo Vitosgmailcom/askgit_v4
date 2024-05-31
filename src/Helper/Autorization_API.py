6aaf185ae869c05dbc08eb45bd6401014a13611a
vitalykamynin <vitaly.kamynin@gmail.com>
2024-05-30 23:02:36 -0400

    15

--- src/Helper/Autorization_API.py
+++ src/Helper/Autorization_API.py
@@ -6,12 +6,12 @@ class Autorization_API:
     def __init__(self):
         self.API_call = Request_API()
 
-    def sign_in_existing_user(self):
+    def sign_in_existing_user(self, expected_status_code=None):
         endpoint = "/sign-in"
         payload = dict()
         payload['email'] = "testuser_erqh@gmail.com"
         payload['password'] = "Password1"
-        result = self.API_call.POST_request(endpoint=endpoint, payload=payload)
+        result = self.API_call.POST_request(endpoint=endpoint, payload=payload, expected_status_code=expected_status_code)
         return result
 
 
--- tests/smoke/test_smoke.py
+++ tests/smoke/test_smoke.py
@@ -9,7 +9,7 @@ class Test_smoke(BaseTest):
     @pytest.mark.smoke_sign_in
     @allure.title('Autorization API smoke test')
     def test_smoke(self):
-        result = Test_smoke.API_CALL.sign_in_existing_user()
+        result = Test_smoke.API_CALL.sign_in_existing_user(expected_status_code=201)
         # import pdb; pdb.set_trace
         with allure.step(f"User is logged in"):
             assert result['user']['active'] == True, f"Expected result is 'True' but API returned '{result['user']['active']}'"
