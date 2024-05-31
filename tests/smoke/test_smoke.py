efba6a9361f7e1b90979e9676c72479cc81dfdf1
vitalykamynin <vitaly.kamynin@gmail.com>
2024-05-30 22:39:24 -0400

    Test11

--- tests/smoke/test_smoke.py
+++ tests/smoke/test_smoke.py
@@ -10,7 +10,7 @@ class Test_smoke(BaseTest):
     @allure.title('Autorization API smoke test')
     def test_smoke(self):
         result = Test_smoke.API_CALL.sign_in_existing_user()
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         assert result['user']['active'] == True, f"Expected result is 'True' but API returned '{result['user']['active']}'"
 
 
